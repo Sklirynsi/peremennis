d73395409058d348e08cec7add453b728c70a073
Sklirynsi <nikolenkosergei.work@gmail.com>
2024-10-30 19:18:37 +0700

    Add files via upload

--- переменные.py
+++ переменные.py
@@ -1,5 +1,5 @@
 Name = 'Python'
-Tasks = ( 12. )
-Hours = ( 1.5 )
-Name = (Hours / Tasks)
-print  ( 'Python', Tasks, Hours, Name )
+total_tasks = (  12 )
+Hours_spent = ( 1.5 )
+Name = (Hours_spent / total_tasks)
+print  ( 'Python, ''всего здач:12,' , 'затрачено часов:1.5, ' , 'среднее время выполнения 0.125 часа' )
