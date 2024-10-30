57dcbedcc1c34418e117bcbc4552a0e13173d092
Sklirynsi <nikolenkosergei.work@gmail.com>
2024-10-30 22:04:01 +0700

    Add files via upload

--- переменные.py
+++ переменные.py
@@ -1,5 +1,5 @@
-Name = 'Python'
+Name = 'курс: ' ' Python'
 total_tasks = (  12 )
 Hours_spent = ( 1.5 )
-Name = (Hours_spent / total_tasks)
-print  ( 'Python, ''всего здач:12,' , 'затрачено часов:1.5, ' , 'среднее время выполнения 0.125 часа' )
+average_execution_time = (Hours_spent / total_tasks)
+print  ( 'курс: ' 'Python,', 'total_tasks : 12,',  'Hours_spent : 1.5,' ' average_execution_time 0.125 ' )
